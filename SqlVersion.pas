291c0dc4f1838c931405255c8c4cced9ada3f29f
Tomasz Gierka <sqltoys@gmail.com>
2019-03-27 11:56:21 +0100

    Project back to work after, test queries first, JOIN keyword missed (query 15).

--- SqlVersion.pas
+++ SqlVersion.pas
@@ -1,4 +1,4 @@
-(* $Header: /SQL Toys/SqlFormat/SqlVersion.pas 367   19-01-23 20:17 Tomek $
+(* $Header: /SQL Toys/SqlFormat/SqlVersion.pas 368   19-01-24 20:37 Tomek $
    (c) Tomasz Gierka, github.com/SqlToys, 2012.09.16                          *)
 {--------------------------------------  --------------------------------------}
 unit SqlVersion;
@@ -103,7 +103,7 @@ end;
 
 function VER_BUILD: string;
 begin
-  Result := '777'; // current build
+  Result := '778'; // current build
 end;
 
 end.
